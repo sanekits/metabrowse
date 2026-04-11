14d103876bd68f6d1790b62196fdbda7b562a0f1
Les Matheson <les.matheson@gmail.com>
2026-04-11 14:01:17 -0400

    Add CUA mode editing

--- src/veditor.d.ts
+++ src/veditor.d.ts
@@ -27,4 +27,8 @@ export function focusEditor(): void;
 export function destroyEditor(): void;
 export function exitInsertMode(): void;
 export function executeExCommand(cmd: string): void;
+export function toggleVimMode(): boolean;
+export function isVimMode(): boolean;
+export function requestSave(): Promise<void>;
+export function requestQuit(force?: boolean): void;
 export function hashTarget(url: string): string;
